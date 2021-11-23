559c62ac0d45a920bb780dd3e460310d22c13a4c
Jéssika Gomes <64968527+jessikagomes@users.noreply.github.com>
2021-11-23 10:35:44 -0300

    first commit class one

---
+++ src/main/java/com/chronosacademy/logica/HelloWorld.java
@@ -0,0 +1,24 @@
+package com.chronosacademy.logica;
+
+public class HelloWorld {
+
+	static String hello;
+	
+	public static void main(String[] args) {
+		
+		System.out.println();
+		
+		
+	}
+
+	public static String getHello() {
+		return hello;
+	}
+
+	public static void setHello(String hello) {
+		HelloWorld.hello = hello;
+	}
+	
+
+
+}
